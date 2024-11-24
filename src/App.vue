3a8238cd8ef94b3057ee03852ac3cce0865c4519
SARENSokmeak <saroussokmeak721@gmail.com>
2024-11-24 18:01:05 +0700

    Say welcome to ip

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
-
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Welcome to Group C3!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-
-  <Home></Home>
-
+  <div class="wrapper">
+    <Home></Home>
+  </div>
 </template>
 
 <script>
