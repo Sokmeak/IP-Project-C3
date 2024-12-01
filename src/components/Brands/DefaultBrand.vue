58c88c84a85c5388c3963f751b97e03fc249d85a
Sokmeak <saroussokmeak721@gmail.com>
2024-12-01 17:48:07 +0700

    include multiple components

---
+++ src/components/Brands/DefaultBrand.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="default-brand">
+    <img src="../../../public/images/defaultLogo.png" alt="aa-logo" />
+    <div class="brand-name">
+      <span :style="{color: fontColor1}">Angkor </span>
+      <span :style="{color: fontColor2}">Apparel</span>
+
+      <!-- <span style="color: #ffdb00">{{ fontColor1 }}</span> -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    fontColor1: String,
+    fontColor2: String,
+  },
+};
+</script>
+<style scoped>
+.default-brand {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 4rem;
+  height: 4rem;
+}
+
+.brand-name {
+  font-family: "Sansita", sans-serif;
+  font-size: 40px;
+  font-weight: 800;
+  color: #26355d;
+  font-style: italic;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <DefaultBranch />
+</template>
+<script>
+import DefaultBranch from "@/components/Brands/DefaultBrand.vue";
+
+export default {
+  components: {
+    DefaultBranch,
+  },
+};
+</script>
+<style></style>
