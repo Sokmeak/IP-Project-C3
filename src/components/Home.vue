646a30f75823efca1f3301e2ca823946e37185fd
Sokmeak <saroussokmeak721@gmail.com>
2024-12-02 16:22:28 +0700

    update main with fonts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <h1>Welcome to Angkor Apparel's Shop Project</h1>
+  <h1>Hello to Home page for Angkor Apparel's Shop Project</h1>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+h1 {
+  font-family: "Roboto", sans-serif;
+  font-weight: 700;
+  font-style: italic;
+}
+</style>
